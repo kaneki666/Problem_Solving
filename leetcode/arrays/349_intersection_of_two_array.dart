2726291268023844508043007cb68c3fa82e8d87
kaneki666 <msadman789@gmail.com>
2022-12-12 14:50:26 +0600

    addintersection

--- leetcode/arrays/349_intersection_of_two_array.dart
+++ leetcode/arrays/349_intersection_of_two_array.dart
@@ -10,12 +10,11 @@ class Solution {
       } else {
         if (i < len1 && result[nums1[i]] == null) {
           result[nums1[i]] = false;
-        } else if (i > len1 - 1 && result[nums1[i]] != null) {
+        } else if (i > len1 && result[nums1[i]] != null) {
           result[nums1[i]] = true;
         }
       }
     }
-
     result.forEach((k, v) => v ? common.add(k) : null);
     return common;
   }
@@ -24,6 +23,9 @@ class Solution {
 void main() {
   Solution s = Solution();
   print(
-    s.intersection([1, 2, 2, 1], [2, 2]),
+    s.intersection(
+      [1, 2, 2, 1],
+      [2, 2],
+    ),
   );
 }
