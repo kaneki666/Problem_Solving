167cc565d93d497d5b29b9c5a9ddce4a9b0c07c9
kaneki666 <msadman789@gmail.com>
2022-12-16 20:41:01 +0600

    addGoalParser

---
+++ leetcode/strings/1678_goal_parser.dart
@@ -0,0 +1,14 @@
+class Solution {
+  String goalParser(String command) {
+    command = command.replaceAll('()', "o");
+    command = (command.replaceAll('(al)', "al"));
+    return command;
+  }
+}
+
+void main() {
+  Solution s = Solution();
+  print(
+    s.goalParser("G()()()()(al)"),
+  );
+}
